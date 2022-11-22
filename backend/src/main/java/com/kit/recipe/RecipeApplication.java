b23b4b058949ee00536d54b10d6a05718899f255
johannes <johannes.uni.kit@gmail.com>
2022-11-22 14:44:17 +0100

    19-Renamed Package

--- backend/src/main/java/com/kit/recipe/RecipeApplication.java
+++ backend/src/main/java/com/kit/recipe/RecipeApplication.java
@@ -1,4 +1,4 @@
-package com.example.recipe;
+package com.kit.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/kit/recipe/RecipeApplicationTests.java
+++ backend/src/test/java/com/kit/recipe/RecipeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.recipe;
+package com.kit.recipe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
