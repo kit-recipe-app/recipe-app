02ad073515e010900e3f95f5ab52c49856192ae2
johannes <johannes.uni.kit@gmail.com>
2022-11-22 17:36:01 +0100

    23-Added-default-routes

---
+++ backend/src/main/java/com/kit/recipe/controller/BaseController.java
@@ -0,0 +1,28 @@
+package com.kit.recipe.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class BaseController {
+
+
+    @GetMapping("/")
+    public ResponseEntity<String> sendHelloWorld() {
+        return ResponseEntity.ok("Hello World");
+    }
+
+    @GetMapping("/einkaufsliste")
+    public ResponseEntity<List<String>> answerPaul() {
+        List<String> shoppingList = List.of("Tomaten", "Gurke", "Bier");
+        return ResponseEntity.ok(shoppingList);
+
+    }
+
+}
