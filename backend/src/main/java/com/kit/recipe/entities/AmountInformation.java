57d7f1183f7b7856b6f5d17b070ee520ae5eb1bb
johan-stph <111685646+johan-stph@users.noreply.github.com>
2022-12-23 13:06:49 +0100

    Changed name of corresponding entity

---
+++ backend/src/main/java/com/kit/recipe/entities/AmountInformation.java
@@ -0,0 +1,35 @@
+package com.kit.recipe.entities;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+public class AmountInformation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "amount_id", nullable = false)
+    private Long id;
+    private String unit;
+    private double amount;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        AmountInformation otherAmountInformation = (AmountInformation) o;
+        return id != null && Objects.equals(id, otherAmountInformation.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
